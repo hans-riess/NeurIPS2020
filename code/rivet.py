38c397e97a8a79ded45cfd6339c16f7eb8a6346d
Hans Matthew Riess <39809993+hans-riess@users.noreply.github.com>
2020-10-06 17:14:55 -0400

    fixed the original commit with colab code so that it runs as it should; still need to double check some things in neural.py

--- code/rivet.py
+++ code/rivet.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 def write_rivet_input(data_matrix,filtration_function,k,name):
     filtration_matrix = filtration_function(data_matrix,k)
@@ -18,3 +19,16 @@ def rivet_find_invariants(data_file_name,output_file_name, h_dim, x_bins,y_bins)
     cmd = "./rivet_console " + data_file_name + " --betti -H "+str(h_dim) + " -x "+str(x_bins)+" -y "+str(y_bins) + " > "+output_file_name
     os.system(cmd)
     return None
+
+def rivet_output_to_features(output_file_name):
+    return None
+
+output_file_name = "training_monitor_h0_0.txt"
+f = open(output_file_name,'r')
+lines = f.readlines()
+for i,line in enumerate(lines):
+    if line == "Dimensions > 0:":
+        break
+print(lines)
+for j in range(i,len(lines)):
+    print(lines[j])
